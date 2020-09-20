0e7a7a5c9f6b2d6664d17260ebb8b98c1744fa2b
mrgawlik4 <mrgawlik@live.unc.edu>
2020-09-20 12:53:50 -0400

    Added JS

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('.my-news-ticker').AcmeTicker({
+  controls: {
+    prev: $('.acme-news-ticker-prev'),
+    toggle: $('.acme-news-ticker-pause'),
+    next: $('.acme-news-ticker-next')
+  }
+});
+
+
+$('.demo').tipso({
+  titleContent:'Channels',
+  background  :'#33cc66',
+  width: 150,
+  titleBackground:'#134670',
+});
+
+$('.demo2').tipso({
+  titleContent:'Referrals',
+  background  :'#33cc66',
+  width: 150,
+  titleBackground:'#134670',
+});
